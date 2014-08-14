d66541fd9424e0a84ab95e37041492b36cb85525
baloodubronx <julien.autebert@gmail.com>
2014-08-14 08:53:37 +0200

    query by pub year

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var http=require('http');
 var str = '';
+var number=1;
 
-http.get('http://www.ebi.ac.uk/europepmc/webservices/rest/search/query=autebert&format=json&resulttype=core', function(response) {
-
+getPMID = function() {
+	var link = 'http://www.ebi.ac.uk/europepmc/webservices/rest/search/format=json&resulttype=core';
+	str='';
+	link += '&query=PUB_YEAR:2013&page='+number;
+	http.get(link, function(response) {
         response.on('data', function (chunk) {
               str += chunk;
         });
-
         response.on('end', function () {
                 var json=JSON.parse(str);
-                console.log(json.resultList.result[0]);
-              
+                console.log(number);
+                number++;
+                if (number < 10) {
+                	getPMID();
+                }
         });
-        
-});
+    });
+};
+
+getPMID();
