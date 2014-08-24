026dce3a2965ab5da950b4360d4091b430e19d5a
baloodubronx <julien.autebert@gmail.com>
2014-08-24 23:07:26 +0200

    Shows articles number

--- functions/pubmed/index.js
+++ functions/pubmed/index.js
@@ -2,6 +2,7 @@ http=require('http');
 
 articles=require('../articles');
 var Journal = require('../journals/journals.model');
+var Article = require('../articles/artciles.model');
 
 
 function getPMIDs(year, page) {
@@ -19,6 +20,9 @@ function getPMIDs(year, page) {
 			articles.create(json.resultList.result);
 			Journal.find().count(function(err, count){
         		console.log(count + ' journals in DB');
+      		});
+      		Article.find().count(function(err, count){
+        		console.log(count + ' articles in DB');
       		});
 			getPMIDs(year, ++page);
 		});
