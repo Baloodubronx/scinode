76511dd86c579912824ef222357c87b8cf895884
baloodubronx <julien.autebert@gmail.com>
2015-03-02 13:01:58 +0100

    still looking for a culprit

--- scrapper/journals/index.js
+++ scrapper/journals/index.js
@@ -28,21 +28,27 @@ exports.clean = function() {
 
 
 exports.makeJournalList = function(year) {
+  console.time('find a suitable article');
   var query = Article.findOne({'processedJournal':false, 'journalInfo.yearOfPublication':year}, function(err, article){
+    console.timeEnd('find a suitable article');
     if (!article) {
 			console.log('no more unprocessed articles FOR JOURNALS (year: '+year+')');
 			process.exit(code=1);
 		}
+    console.time('the math thingy');
     if (Math.floor(Math.random()*10)>8){
       Article.find({'processedJournal':false, 'journalInfo.yearOfPublication':year}).count(function(err, count) {
         console.log(count+' more to do in '+year);
       });
     }
+    console.timeEnd('the math thingy');
     var count = 0;
     if (article.citedByCount) {
       count = article.citedByCount;
     }
+    console.time('create journal from infos');
     exports.create(article.journalInfo.journal.toJSON(), count, function(){
+      console.timeEnd('create journal from infos');
       article.processedJournal=true;
       console.time('save article');
       article.save(function() {
