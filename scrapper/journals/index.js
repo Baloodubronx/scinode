95ef61aaccad9e12bed843991dbb34630f4906fd
baloodubronx <julien.autebert@gmail.com>
2015-03-02 13:12:27 +0100

    artichaud

--- scrapper/journals/index.js
+++ scrapper/journals/index.js
@@ -27,32 +27,25 @@ exports.clean = function() {
 
 
 
-exports.makeJournalList = function(year) {
-  console.time('find a suitable article');
+exports.makeJournalList = function(year, articount) {
   var query = Article.findOne({'processedJournal':false, 'journalInfo.yearOfPublication':year}, function(err, article){
-    console.timeEnd('find a suitable article');
     if (!article) {
 			console.log('no more unprocessed articles FOR JOURNALS (year: '+year+')');
 			process.exit(code=1);
 		}
-    console.time('the math thingy');
-    if (Math.floor(Math.random()*10)>8){
-      Article.find({'processedJournal':false, 'journalInfo.yearOfPublication':year}).count(function(err, count) {
-        console.log(count+' more to do in '+year);
-      });
+    if (Math.floor(Math.random()*100)>95){
+      articount +=1;
+      console.log(articount);
     }
-    console.timeEnd('the math thingy');
+
     var count = 0;
     if (article.citedByCount) {
       count = article.citedByCount;
     }
-    console.time('create journal from infos');
+
     exports.create(article.journalInfo.journal.toJSON(), count, function(){
-      console.timeEnd('create journal from infos');
-      console.time('save article');
       Article.findOneAndUpdate({pmid:article.pmid}, {'processedJournal':true}, function(err){
-        console.timeEnd('save article');
-        exports.makeJournalList(year);
+        exports.makeJournalList(year, articount);
       });
     });
   });
