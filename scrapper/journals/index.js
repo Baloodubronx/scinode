48cb03c9b46165be7ab406d7469b08c7ff3e794a
baloodubronx <julien.autebert@gmail.com>
2015-03-02 12:45:32 +0100

    console time

--- scrapper/journals/index.js
+++ scrapper/journals/index.js
@@ -4,6 +4,7 @@ var Article = require('../../models/articles.model');
 
 // Creates a new thing in the DB.
 exports.create = function(journal, citations, callback) {
+  console.time('finding one journal');
   Journal.findOne({'nlmid': journal.nlmid}, function(err, journalfound){
     if (err) return console.log(err);
     if (journalfound) {
@@ -28,6 +29,7 @@ exports.clean = function() {
 
 
 exports.makeJournalList = function(year) {
+  console.time('finding one article');
   var query = Article.findOne({'processedJournal':false, 'journalInfo.yearOfPublication':year}, function(err, article){
     if (!article) {
 			console.log('no more unprocessed articles FOR JOURNALS (year: '+year+')');
@@ -38,12 +40,14 @@ exports.makeJournalList = function(year) {
         console.log(count+' more to do in '+year);
       });
     }
+    console.timeEnd('finding one article');
     var count = 0;
     if (article.citedByCount) {
       count = article.citedByCount;
     }
     exports.create(article.journalInfo.journal.toJSON(), count, function(){
       article.processedJournal=true;
+      console.timeEnd('finding one journal');
       article.save(function() {
         exports.makeJournalList(year);
       });
