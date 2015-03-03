235c483de7f0f1fae24ea5dde1b76d9731cb7a0a
baloodubronx <julien.autebert@gmail.com>
2015-03-03 21:14:37 +0100

    process env

--- scrapper/makejournalist.js
+++ scrapper/makejournalist.js
@@ -3,4 +3,4 @@ var journals = require('./journals');
 
 mongoose.connect('mongodb://localhost/scrapper');
 
-journals.makeJournalList(2014, 0);
+journals.makeJournalList(process.env.YEAR || 2014, 0);
